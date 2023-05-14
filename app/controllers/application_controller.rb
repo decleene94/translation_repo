a8a192f7f6320d56b4ac1e34ed4ef98ae9e364fb
John DeCleene <decleene94@gmail.com>
2023-05-14 09:41:42 +0800

    adding views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,28 @@
 class ApplicationController < ActionController::Base
-end
 
-#comment
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  # ...
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/posts', to: 'application#index'
+  get '/posts/:id', to: 'application#show'
+  post '/posts', to: 'application#create'
 end
